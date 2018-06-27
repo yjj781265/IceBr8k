5c72e27d833471865e2191e137b154964c4d15b1
yjj781265 <yjj950406@gmail.com>
2018-06-26 21:13:18 -0400

    add feedback connect button and tutorial ui

--- app/src/main/java/app/jayang/icebr8k/Fragments/SurveyTab_Fragment.java
+++ app/src/main/java/app/jayang/icebr8k/Fragments/SurveyTab_Fragment.java
@@ -1,12 +1,15 @@
 package app.jayang.icebr8k.Fragments;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
@@ -29,6 +32,7 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import com.beardedhen.androidbootstrap.BootstrapButton;
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.dd.processbutton.FlatButton;
 import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
 import com.github.javiersantos.materialstyleddialogs.enums.Style;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -49,12 +53,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
+import app.jayang.icebr8k.Homepage;
 import app.jayang.icebr8k.Modle.CommentBottomSheetFrag;
 import app.jayang.icebr8k.Modle.SurveyQ;
-import app.jayang.icebr8k.Modle.UserDialog;
 import app.jayang.icebr8k.Modle.UserQA;
+import app.jayang.icebr8k.PeopleNearby;
 import app.jayang.icebr8k.R;
 import app.jayang.icebr8k.Utility.Compatability;
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
 
 
 /**
@@ -67,6 +75,7 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
     private TextView mTextView, msubTextview, progressText;
 
     private RadioGroup mRadioGroup;
+    private Homepage mHomepage;
     private ImageView  thumbUp, thumbDown;
     private RelativeLayout loadingGif;
     private BootstrapButton mSubmit;
@@ -74,10 +83,11 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
     private UserQA  currentUserQA;
     private TextView skip;
     private int index =0, ChilderenCounter =0 , Counter=0;
-    private FloatingActionButton mActionButton;
     private MaterialDialog loadingDialog;
     private String TAG = "surveyFrag";
     private CardView cardView;
+    private ConstraintLayout feedbackPannel;
+    private FlatButton connect, feedback,moreQuestion;
     private DatabaseReference userQARef;
     private final String MC_TYPE = "mc";
     private final String SC_TYPE = "sc";
@@ -142,14 +152,17 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
         progressText.setVisibility(View.INVISIBLE);
 
 
-        mActionButton= mview.findViewById(R.id.floatingActionButton);
-        mActionButton.setVisibility(View.GONE);
+
         skip = mview.findViewById(R.id.skip_btn);
         skip.setVisibility(View.GONE);
         cardView = mview.findViewById(R.id.cardView);
         cardView.setVisibility(View.GONE);
         loadingGif = mview.findViewById(R.id.survey_loading);
         loadingGif.setVisibility(View.VISIBLE);
+        feedbackPannel = mview.findViewById(R.id.survey_feedbackPannel);
+        moreQuestion = mview.findViewById(R.id.survey_moreq);
+        feedback = mview.findViewById(R.id.survey_feedback);
+        connect = mview.findViewById(R.id.survey_connect);
 
         loadingDialog = new MaterialDialog.Builder(getContext())
 
@@ -203,15 +216,62 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
 
         });
 
-
-
-        mActionButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mActionButton.setClickable(false);// prevent user spam click the button ,may crash the program
+        moreQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 mTextView.setVisibility(View.INVISIBLE);
                 initQuestions();
+                if(surveyList.isEmpty()){
+                    Toast.makeText(getActivity(), "You have answered all the questions", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if share my postion is on
+                if("public".equals(getPrivacySharedPreference())){
+                    showSingleChoiceDialog();
+                }else {
+                    new MaterialDialog.Builder(getActivity())
+                            .content("\"Share My Location\" is disabled, you can go to Me tab to enable it.")
+                            .positiveText(R.string.ok).onPositive(new MaterialDialog.SingleButtonCallback() {
+                        @Override
+                        public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                            mHomepage = (Homepage)getActivity();
+                            if(mHomepage!=null) {
+                                mHomepage.getViewPager().setCurrentItem(4,true);
+                            }
+
+                        }
+                    })
+                            .show();
+                }
+            }
+        });
+
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("market://details?id=" + getActivity().getPackageName());
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + getActivity().getPackageName())));
+                }
+            }
+        });
+
+
+
 
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -293,6 +353,8 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
 
 
     public void initQuestions(){
+        feedbackPannel.setVisibility(View.GONE);
+        cardView.setVisibility(View.VISIBLE);
     userQlist = new ArrayList<>();
     index =0;
     mProgressBar.setIndeterminate(true);
@@ -409,18 +471,25 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if(!surveyList.isEmpty()){
-                    mActionButton.setVisibility(View.GONE);
                     Collections.shuffle(surveyList);
                     updateUI(surveyList.get(0));
+                    cardView.setVisibility(View.VISIBLE);
+
+
+
+
+
+
+                    // no more questions in database
                 }else if(surveyList.isEmpty()){
-                    mTextView.setText(getString(R.string.no_question_left));
                     hideCardViewComponent();
+
 
                 }
                 mProgressBar.setVisibility(View.GONE);
                 loadingGif.setVisibility(View.GONE);
 
-                cardView.setVisibility(View.VISIBLE);
+
 
             }
 
@@ -432,6 +501,7 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
     }
 
     private void updateUI(SurveyQ surveyQ) {
+
         if(surveyList.indexOf(surveyQ) ==0){
             backArrow.setVisibility(View.GONE);
         }else{
@@ -952,21 +1022,47 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
 
 
     private void hideCardViewComponent(){
-        mTextView.setText(getString(R.string.no_question_left));
-        mTextView.setTypeface(Typeface.DEFAULT);
+        feedbackPannel.setVisibility(View.VISIBLE);
+        cardView.setVisibility(View.GONE);
+       mTextView.setVisibility(View.GONE);
         progressText.setVisibility(View.GONE);
-        mTextView.setVisibility(View.VISIBLE);
         mSpinner.setVisibility(View.GONE);
         mProgressBar.setVisibility(View.INVISIBLE);
         mRadioGroup.setVisibility(View.GONE);
         mSubmit.setVisibility(View.GONE);
         msubTextview.setVisibility(View.GONE);
         mSeekBar.setVisibility(View.INVISIBLE);
-        mActionButton.setVisibility(View.VISIBLE);
-        mActionButton.setClickable(true);
         skip.setVisibility(View.GONE);
         backArrow.setVisibility(View.GONE);
         progressText.setVisibility(View.GONE);
+
+        //reset tutorial
+        MaterialShowcaseView.resetSingleUse(getActivity(), "survey_tutorial");
+
+        // sequence example
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(500); // half second between each showcase view
+
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(getActivity(), "survey_tutorial");
+
+        sequence.setConfig(config);
+
+        sequence.addSequenceItem(moreQuestion,
+                "Click here to load more questions", "GOT IT");
+
+        sequence.addSequenceItem(connect,
+                "Click here to connect with people nearby", "GOT IT");
+
+        sequence.addSequenceItem(feedback,
+                "Click here to give us feedback on Google Play Store , we want to make this app better for everyone", "GOT IT");
+
+        sequence.start();
+
+
+
+
+
 
 
 
@@ -1090,22 +1186,37 @@ public class SurveyTab_Fragment extends Fragment implements OnSuccessListener {
 
     }
 
-    protected  class UpdateCompatability extends  AsyncTask<Void,Void,Void>{
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            loadingDialog.dismiss();
-
-
-        }
-
-
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            compareWithFriends();
-            return null;
-        }
+    private String getPrivacySharedPreference(){
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = "private";
+        String privacy = sharedPref.getString(currentUser.getUid()+"privacy", defaultValue);
+        return privacy;
     }
+
+    private void showSingleChoiceDialog() {
+
+        new MaterialDialog.Builder(getContext())
+                .title(R.string.radius_title)
+                .items(R.array.radius)
+                .itemsCallbackSingleChoice(index, new MaterialDialog.ListCallbackSingleChoice() {
+                    @Override
+                    public boolean onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                        index =which;
+                        String radius = String.valueOf(text);
+                        Intent intent = new Intent(getActivity(),PeopleNearby.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        intent.putExtra("radius",radius);
+                        intent.putExtra("index",index);
+                        startActivity(intent);
+                        return true;
+                    }
+                })
+                .positiveText(R.string.ok).show();
+    }
+
+
+
+
 
 
 
